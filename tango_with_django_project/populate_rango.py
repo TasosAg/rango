65677a77e8addd3a91ba7deb3339f35718a01c0c
Tasos <tasosagathokleous@gmail.com>
2017-01-24 02:20:32 +0000

    chapter6

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -12,6 +12,7 @@ def populate():
 # Then we will create a dictionary of dictionaries for our categories.
 # This might seem a little bit confusing, but it allows us to iterate
 # through each data structure, and add the data to our models.
+
     python_pages = [
         {"title": "Official Python Tutorial",
         "url":"http://docs.python.org/2/tutorial/"},
@@ -27,6 +28,7 @@ def populate():
         "url":"http://www.djangorocks.com/"},
         {"title":"How to Tango with Django",
         "url":"http://www.tangowithdjango.com/"} ]
+
     other_pages = [
         {"title":"Bottle",
         "url":"http://bottlepy.org/docs/dev/"},
@@ -39,18 +41,14 @@ def populate():
 # If you want to add more catergories or pages,
 
 # add them to the dictionaries above.
-#
-#
-#
-#
-#
+
 #The code below goes through the cats dictionary, then adds each category,
 #and then adds all the associated pages for that category.
 #if you are using Python 2.x then use cats.iteritems() see
 #http://docs.quantifiedcode.com/python-anti-patterns/readability/
 #for more information about how to iterate over a dictionary properly.
 
-    for cat, cat_data in cats.items():
+    for cat, cat_data in cats.iteritems():
         c = add_cat(cat)
         for p in cat_data["pages"]:
             add_page(c, p["title"], p["url"])
@@ -68,7 +66,12 @@ def add_page(cat, title, url, views=0):
     return p
 
 def add_cat(name):
-    c = Category.objects.get_or_create(name=name, views=views, likes=likes)[0]
+    c = Category.objects.get_or_create(name=name)[0]
+    c.views
+    c.likes
+    #c.name = name
+    #c.views=views
+    #c.likes=likes
     c.save()
     return c
 # Start execution here!
---
+++ tango_with_django_project/rango/rango_tests-master/README.md
@@ -0,0 +1,20 @@
+# Rango Tests for WAD2 and ITECH
+
+You can run these tests as:
+
+`python manage.py test rango.tests_chapter3`
+
+`python manage.py test rango.tests_chapter4`
+
+`...`
+
+`python manage.py test rango.tests_chapter10`
+
+These tests are only valid for the Rango app of the Tango with Django Book (https://leanpub.com/tangowithdjango19/) up to Chapter 10. Each test runs view and model tests. If you are in Chapter 6, tests from previous chapters might fail as the structure of templates/views changes as you develop Rango.
+
+# Requirements:
+
+* Python 2.7
+* Django-1.10
+* Pillow-3.3.1
+
