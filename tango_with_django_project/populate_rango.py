000205f8c62f0e3a6b3a7732b8d32043eb01cb87
Tasos <tasosagathokleous@gmail.com>
2017-01-24 03:59:17 +0000

    chapter5b

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -12,6 +12,7 @@ def populate():
 # Then we will create a dictionary of dictionaries for our categories.
 # This might seem a little bit confusing, but it allows us to iterate
 # through each data structure, and add the data to our models.
+
     python_pages = [
         {"title": "Official Python Tutorial",
         "url":"http://docs.python.org/2/tutorial/"},
@@ -27,30 +28,27 @@ def populate():
         "url":"http://www.djangorocks.com/"},
         {"title":"How to Tango with Django",
         "url":"http://www.tangowithdjango.com/"} ]
+
     other_pages = [
         {"title":"Bottle",
         "url":"http://bottlepy.org/docs/dev/"},
         {"title":"Flask",
         "url":"http://flask.pocoo.org"} ]
 
-    cats = {"Python": {"pages": python_pages},
-        "Django": {"pages": django_pages},
-        "Other Frameworks": {"pages": other_pages} }
+    cats = {"Python": {"pages": python_pages, "likes": 64 , "views": 128},
+        "Django": {"pages": django_pages, "likes":32 , "views":64 },
+        "Other Frameworks": {"pages": other_pages, "likes":16 , "views":32 } }
 # If you want to add more catergories or pages,
 
 # add them to the dictionaries above.
-#
-#
-#
-#
-#
+
 #The code below goes through the cats dictionary, then adds each category,
 #and then adds all the associated pages for that category.
 #if you are using Python 2.x then use cats.iteritems() see
 #http://docs.quantifiedcode.com/python-anti-patterns/readability/
 #for more information about how to iterate over a dictionary properly.
 
-    for cat, cat_data in cats.items():
+    for cat, cat_data in cats.iteritems():
         c = add_cat(cat)
         for p in cat_data["pages"]:
             add_page(c, p["title"], p["url"])
@@ -68,7 +66,11 @@ def add_page(cat, title, url, views=0):
     return p
 
 def add_cat(name):
-    c = Category.objects.get_or_create(name=name, views=views, likes=likes)[0]
+    c = Category.objects.get_or_create(name=name)[0]
+    #for c in cat_data["liike"]:
+    #    add_page(c, p["title"], p["url"])
+    c.likes=64
+    c.views=128
     c.save()
     return c
 # Start execution here!
